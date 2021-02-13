f5e11cfca6a4554aa822dc825bcbb109139cf94f
anaiancu24 <ana.iulia24@icloud.com>
2021-02-13 21:31:27 +0100

    Refactoring + fetch user

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,32 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
+
+  renderContent() {
+    switch(this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li><a href="/auth/google">Log in with Google</a></li>
+        );
+      default:
+        return <li><a href="/api/logout">Logout</a></li>;
+    }
+  }
+
     render() {
         return (
             <nav>
             <div className="nav-wrapper">
-              <a href="/" className="brand-logo">Emaily</a>
-              <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="sass.html">Login with Google</a></li>
+              <Link 
+              to={this.props.auth ? '/surveys' : '/'} 
+              className="left brand-logo">Emaily
+              </Link>
+              <ul id="nav-mobile" className="right">
+                {this.renderContent()}
               </ul>
             </div>
           </nav>
@@ -16,4 +35,8 @@ class Header extends Component {
     }
 }
 
-export default Header;
+function mapStateToProps({auth}) {
+  return {auth};
+}
+
+export default connect(mapStateToProps)(Header);
