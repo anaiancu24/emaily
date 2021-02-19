ae85fdfb7e06c8185f30c16765e07a2786d4866a
anaiancu24 <ana.iulia24@icloud.com>
2021-02-19 23:30:56 +0100

    Stripe setup client side

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
 
@@ -13,7 +14,10 @@ class Header extends Component {
           <li><a href="/auth/google">Log in with Google</a></li>
         );
       default:
-        return <li><a href="/api/logout">Logout</a></li>;
+        return [
+          <li key="1"><Payments /></li>,
+          <li key="2"><a href="/api/logout">Logout</a></li>
+      ];
     }
   }
 
@@ -26,7 +30,7 @@ class Header extends Component {
               className="left brand-logo">Emaily
               </Link>
               <ul id="nav-mobile" className="right">
-                {this.renderContent()}
+                { this.renderContent() }
               </ul>
             </div>
           </nav>
