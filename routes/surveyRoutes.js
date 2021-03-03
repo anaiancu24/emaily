d88c70e32f01a5d1b47ffb47fbb80defde438db0
anaiancu24 <ana.iulia24@icloud.com>
2021-03-03 21:54:48 +0100

    Setup sendgrid

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin')
 const requireCredits = require('../middlewares/requireCredits')
+const Mailer = require('../services/Mailer')
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
 
-const Survey = mongoose.model('survey');
+const Survey = mongoose.model('surveys');
 
 module.exports = app => {
     app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
@@ -16,5 +18,9 @@ module.exports = app => {
             _user: req.user.id,
             dateSent: Date.now(),
         })
+
+        // Great place to send an email
+        const mailer = new Mailer(survey, surveyTemplate(survey));
+
     });
 }
